72d0426846e89aa752f599572b44b0e7b4153edb
lulujing <893905340@qq.com>
2017-10-05 00:21:19 +1300

    2017-10-5Create project structure

---
+++ MS_MVC_JavaScript_WEbPage/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MS_MVC_JavaScript_WEbPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MS_MVC_JavaScript_WEbPage/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MS_MVC_JavaScript_WEbPage.MvcApplication" Language="C#" %>
